59ca65cc5d38ee3a9a3ade956bdd73dd57bd7d85
Hawkins <haakenson@gmail.com>
2018-12-04 12:46:01 -0800

    clean up and additional comments

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,9 +2,10 @@
 let currentPress = '';
 let currentPressID = '';
 let gamepadCurrent = null;
-let poller = null;
+let poller = null; // timer identifier, allows removal of setInterval when gamepad is disconnected
 const socket = io.connect('//localhost:3000/');
-const tranDuration = 333;
+const tranDuration = 333; // duration of .active class transition
+// map of gamepad button number: html element id
 const keyMap = {
     0: 'button-A',
     1: 'button-B',
@@ -27,25 +28,27 @@ const keyMap = {
 
 /// jQuery events ///
 $('.pressable')
-    .on('keypress', function(e) {
-        e.preventDefault();
+    .on('keypress', function(e) { // keypress
+        e.preventDefault(); // don't actually follow hrefs
         currentPressID = this.id;
         currentPress = currentPressID.split('-')[1];
 
+        // highlight button press for a short time
         $(this).addClass('active');
         setTimeout(() => $(this).removeClass('active'), tranDuration);
     })
-    .on('click', function(e) { // keypress/enter
-        e.preventDefault();
+    .on('click', function(e) { // mouse click/enter
+        e.preventDefault(); // don't actually follow hrefs
         currentPressID = this.id;
         currentPress = currentPressID.split('-')[1];
 
+        // highlight button press for a short time
         $(this).addClass('active');
         setTimeout(() => $(this).removeClass('active'), tranDuration);
 
-        // trying to differentiate mouse clicks from hitting enter
-        if (e.screenX === 0 && e.screenY === 0) { // enter
-        } else { // mouse click
+        // trying to differentiate mouse clicks from hitting enter, so we don't
+        // affect accessibility for keyboard users (by calling .blur())
+        if (e.screenX !== 0 || e.screenY !== 0) { // mouse click
             setTimeout(() => $(this).blur(), tranDuration);
         }
     });
@@ -56,11 +59,12 @@ const setScale = () => {
     let $wrapper = $('#wrapper');
     // 1024px x 768px is the default size of #gamepad
     let scaleW = window.innerWidth / 1024;
-    let scaleH = window.innerHeight / 768 / 2;
-    let scale = scaleH < scaleW ? scaleH : scaleW;
+    let scaleH = window.innerHeight / 768 / 2; // divide by 2 because height is 50%
+    let scale = scaleH < scaleW ? scaleH : scaleW; // choose smaller of the scales to ensure fit
     $('#gamepad').css({
         'transform': 'scale(' + scale + ')'
     });
+    // visibility starts hidden to hide sudden 'jumps' in controller scale before sizing
     $wrapper.css({
         'width': 1024 * scale + 'px',
         'height': 768 * scale + 'px',
@@ -81,6 +85,7 @@ if (rando > .666) {
 
 /// Socket.io incoming ///
 socket.on('winning-button', function (data) {
+    // display winning button overlayed on video output, then fade
     $('#output').append('<span>' + data.button + '</span>');
     setTimeout(() => $('#output>span:first-child').remove(), 4800);
 });
@@ -92,26 +97,21 @@ const sendPress = () => {
         socket.emit('press', {
             'button': currentPress
         });
+        // highlight sent button press
         $('#' + currentPressID).addClass('sentPress');
         setTimeout(() => $('.pressable').removeClass('sentPress'), tranDuration);
+        // reset current button press
         currentPress = '';
         currentPressID = '';
     }
 };
-setInterval(sendPress, 1000); // only send button press every 1000ms
+setInterval(sendPress, 1000); // only send button press once every 1000ms
 
 
 /// Gamepad API ///
 function gamepadPoller() {
-    console.debug('started');
-//        console.debug('buttons: ' + gamepadCurrent.buttons.length);
-    /*
-    for (let i = 0; i < 4; i++) {
-        console.debug(navigator.getGamepads()[i]);
-    }
-    */
-
-
+    // scan buttons in order until a pressed one is found
+    // (i.e. when multiple buttons are pressed, only one will count)
     for (let i = 0; i < navigator.getGamepads()[gamepadCurrent].buttons.length; i++) {
         let val = navigator.getGamepads()[gamepadCurrent].buttons[i];
 
@@ -122,69 +122,33 @@ function gamepadPoller() {
             val = val.value;
         }
 
+        // check the next button, if this one wasn't pressed
         if (!pressed) continue;
-        console.debug(i + ': ' + pressed);
-        console.debug(i + ': ' + val);
+
+        // use the already-present click handler on buttons
         $('#' + keyMap[i]).click();
         break;
-
-//            console.debug(i + ': ' + navigator.getGamepads()[2].buttons[i].pressed);
-//            if (gamepadCurrent.buttons[i].pressed) {
-//                console.debug('i: ' + i);
-//            }
     }
-    console.debug('ended');
 }
 
+// when a gamepad is connected, check that it is real (that it actually has buttons)
+// then start the poller to check that gamepad
 window.addEventListener("gamepadconnected", function(e) {
-    console.debug('connect');
-    /*
-    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-        e.gamepad.index, e.gamepad.id,
-        e.gamepad.buttons.length, e.gamepad.axes.length);
-        */
-
     if (e.gamepad !== null
         && e.gamepad.mapping === 'standard'
         && e.gamepad.buttons.length > 0
         && gamepadCurrent === null) {
-            gamepadCurrent = e.gamepad.index;
-            console.debug('found');
-            console.table(gamepadCurrent);
+            // if a previous gamepad poller is running, stop it and start a new one
             if (poller !== null) {
                 clearInterval(poller);
-                console.debug('poller: ' + poller);
             }
-            poller = setInterval(gamepadPoller, 83);
-//      rAF(gamepadPoller);
-            console.debug('poller: ' + poller);
+            gamepadCurrent = e.gamepad.index;
+            poller = setInterval(gamepadPoller, 83); // ~12 times/second
     }
 });
-/*
-$(window).on('gamepadconnected', function(e) {
-    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-        e.gamepad.index, e.gamepad.id,
-        e.gamepad.buttons.length, e.gamepad.axes.length);
-});
-*/
 
+// on a gamepad disconnect, stop the polling function, and nullify the current reference
 window.addEventListener("gamepaddisconnected", function(e) {
     clearInterval(poller);
     gamepadCurrent = null;
-    console.log("Gamepad disconnected from index %d: %s",
-        e.gamepad.index, e.gamepad.id);
 });
-
-/*
-let gamepads = navigator.getGamepads();
-console.table(gamepads);
-for (let i = 0; i < gamepads.length; i++) {
-    if (gamepads[i] !== null
-        && gamepads[i].mapping === 'standard'
-        && gamepads[i].buttons.length > 0) {
-        console.log(gamepads[i]);
-    } else {
-        console.log("nope");
-    }
-}
-*/
