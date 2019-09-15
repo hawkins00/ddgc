e005edc6b33a569f586ae1f1a9b056917ca29cb4
Hawkins <haakenson@gmail.com>
2019-09-15 17:30:07 -0500

    fixed title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DDGC' });
+  res.render('index', { title: 'DGC' });
 });
 
 module.exports = router;
